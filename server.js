6e27d282327332dea45eadc9d67721af6581144a
Sebastian Drzewicki <ddosdor@gmail.com>
2021-07-26 00:36:02 +0200

    (chore:fix): Vercel... again

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ const manifest = require(path.join(__dirname, 'server/ssr-manifest.json'));
 
 const server = express();
 
-const appPath = path.join(__dirname, '/', 'server', manifest['main.js']);
+const appPath = path.join(__dirname, '', 'server', manifest['main.js']);
 // eslint-disable-next-line import/no-dynamic-require
 const createApp = require(appPath).default;
 
-server.use('/img', express.static(path.join(__dirname, '/client', 'img')));
-server.use('/js', express.static(path.join(__dirname, '/client', 'js')));
-server.use('/css', express.static(path.join(__dirname, '/client', 'css')));
+server.use('/img', express.static(path.join(__dirname, 'client', 'img')));
+server.use('/js', express.static(path.join(__dirname, 'client', 'js')));
+server.use('/css', express.static(path.join(__dirname, 'client', 'css')));
 server.use(
   '/favicon.ico',
-  express.static(path.join(__dirname, '/client', 'favicon.ico')),
+  express.static(path.join(__dirname, 'client', 'favicon.ico')),
 );
 
 server.get('*', async (req, res) => {
@@ -27,7 +27,7 @@ server.get('*', async (req, res) => {
 
   const appContent = await renderToString(app);
 
-  fs.readFile(path.join(__dirname, '/client/index.html'), (err, html) => {
+  fs.readFile(path.join(__dirname, 'client/index.html'), (err, html) => {
     if (err) {
       throw err;
     }
